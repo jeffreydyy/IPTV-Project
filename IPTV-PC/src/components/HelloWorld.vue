4b8f09c632ec656fdaca0b349d85c8d8818260a5
杨扬 <306869344@qq.com>
2019-02-20 14:07:50 +0800

    201902201407

---
+++ IPTV-PC/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>123</div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
